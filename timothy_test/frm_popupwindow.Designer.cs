fe09dc871fa37eb7f50737fc7ccac876d73759ca
akshayamhaskar <work.akshayamh@gmail.com>
2016-02-19 19:59:41 +0530

    made UI changes in popup window

--- timothy_test/frm_popupwindow.Designer.cs
+++ timothy_test/frm_popupwindow.Designer.cs
@@ -28,46 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_popupwindow));
             this.lst_main = new System.Windows.Forms.ListBox();
             this.btn_prev = new System.Windows.Forms.Button();
             this.btn_next = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // lst_main
             // 
-            this.lst_main.BackColor = System.Drawing.SystemColors.Info;
+            this.lst_main.BackColor = System.Drawing.Color.Wheat;
+            this.lst_main.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lst_main.FormattingEnabled = true;
-            this.lst_main.Location = new System.Drawing.Point(84, -2);
+            this.lst_main.ItemHeight = 18;
+            this.lst_main.Location = new System.Drawing.Point(139, 12);
             this.lst_main.Name = "lst_main";
-            this.lst_main.Size = new System.Drawing.Size(185, 264);
+            this.lst_main.Size = new System.Drawing.Size(250, 310);
             this.lst_main.TabIndex = 0;
             this.lst_main.SelectedIndexChanged += new System.EventHandler(this.lst_main_SelectedIndexChanged);
             // 
             // btn_prev
             // 
-            this.btn_prev.Location = new System.Drawing.Point(3, 91);
+            this.btn_prev.BackColor = System.Drawing.Color.Transparent;
+            this.btn_prev.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_prev.BackgroundImage")));
+            this.btn_prev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_prev.Location = new System.Drawing.Point(53, 135);
             this.btn_prev.Name = "btn_prev";
-            this.btn_prev.Size = new System.Drawing.Size(75, 23);
+            this.btn_prev.Size = new System.Drawing.Size(42, 41);
             this.btn_prev.TabIndex = 1;
-            this.btn_prev.Text = "previous";
-            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.UseVisualStyleBackColor = false;
             this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
             // 
             // btn_next
             // 
-            this.btn_next.Location = new System.Drawing.Point(275, 91);
+            this.btn_next.BackColor = System.Drawing.Color.Transparent;
+            this.btn_next.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_next.BackgroundImage")));
+            this.btn_next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_next.Location = new System.Drawing.Point(427, 135);
             this.btn_next.Name = "btn_next";
-            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.Size = new System.Drawing.Size(41, 41);
             this.btn_next.TabIndex = 2;
-            this.btn_next.Text = "next";
-            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.UseVisualStyleBackColor = false;
             this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // frm_popupwindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(350, 261);
+            this.ClientSize = new System.Drawing.Size(551, 343);
             this.Controls.Add(this.btn_next);
             this.Controls.Add(this.btn_prev);
             this.Controls.Add(this.lst_main);
@@ -84,5 +99,6 @@
         private System.Windows.Forms.ListBox lst_main;
         private System.Windows.Forms.Button btn_prev;
         private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
